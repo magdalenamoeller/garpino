cfce92d3f83e98445c9706d839699808769d002a
magdalena <magdalena.moeller@gmail.com>
2015-08-26 16:20:28 +0200

    first commit

---
+++ podlewanie.ino
@@ -0,0 +1,30 @@
+
+//sensory analogowe, mierza wilgotnosc
+const int aHumidityPin0 = A0;
+const int aHumidityPin1 = A1;
+const int aHumidityPin2 = A2;
+const int aHumidityPin3 = A3;
+
+//przyciski, dzialaja na pull down, zmacknuti-high
+const int aButton4 = A4;
+const int aButton5 = A5;
+const int dButton6 = D6;
+const int dButton7 = D7;
+
+//silniczki
+const int dMotor2 = D2;
+const int dMotor3 = D3;
+const int dMotor4 = D4;
+const int dMotor5 = D5;
+
+//wyswietlacz, data: 11, 10, 9, 8
+const int data8 = 8;
+const int data9 = 9;
+const int data10 = 10;
+const int data11 = 11;
+
+//řídící
+const int control12 = 12
+const int control13 = 13;
+
+
