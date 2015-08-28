b2fb8dc4c749adb29a7220d4add4d1be1e0195ff
magdalena <magdalena.moeller@gmail.com>
2015-08-28 17:31:10 +0200

    add clock to LCD, turn on motors on low humidity

--- garpino.ino
+++ garpino.ino
@@ -1,6 +1,6 @@
 #include <Time.h>
 #define TIME_MSG_LEN  11   // time sync to PC is HEADER followed by Unix time_t as ten ASCII digits
-#define TIME_HEADER  'T'   // Header tag for serial time sync message
+#define TIME_HEADER  "T"   // Header tag for serial time sync message
 #define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
 
 
@@ -26,7 +26,17 @@
 
 LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
 
-unsigned long time1 = 1;         
+time_t measurement_time = 0;
+time_t start_time = 0;
+
+#define HOURS_8 8//28800000 //default interval for measurements
+#define HUMIDITY_THRESHOLD 400
+static uint32_t measurmentInterval = 20;//28800;  in seconds!
+static uint32_t waterInterval = 5; //in seconds
+
+int a0, a1, a2, a3; //analog input humidity values
+
+boolean isPumpingWater = false;
 
 void setup() {
   Serial.begin(57600);
@@ -41,15 +51,73 @@ void setup() {
   pinMode(MOTOR1, OUTPUT);
   pinMode(MOTOR2, OUTPUT);
   pinMode(MOTOR3, OUTPUT);
-  
+
   setSyncProvider( requestSync);  //set function to call when sync required
   Serial.println("Waiting for sync message");
+
 }
 
 void loop() {
   delay(100);
   Serial.println("Ahoj Svete");
 
+  syncTimeOnSerial();
+
+  if( isPumpingWater == false ) {
+    serveTheButtons();
+  }
+
+  lcd.clear();
+  time_t now_t = now();
+  digitalClockLCD(now_t, 0);
+
+  if(isTimeToMeasure()) {
+    a0 = analogRead(HUMIDITY_PIN0);
+    a1 = analogRead(HUMIDITY_PIN1);
+    a2 = analogRead(HUMIDITY_PIN2);
+    a3 = analogRead(HUMIDITY_PIN3); 
+
+
+    if(isTooDry(a0)) {
+      start_time = now();
+      digitalWrite(MOTOR0, HIGH); //put the water there
+      isPumpingWater = true;
+    }
+  }
+  
+  showLastMeasurement(a0,a1,a2,a3);
+  digitalClockLCD(start_time, 2);
+   
+  if (isPumpingWater && isTimeToStopMotor()) {
+    digitalWrite(MOTOR0, LOW);
+    isPumpingWater = false;
+  }
+
+}//end loop
+
+
+boolean isTooDry(int aHumidity) {
+  if (  aHumidity < HUMIDITY_THRESHOLD ) {
+    return true;
+  } 
+  else {
+    return false;
+  }
+}
+
+boolean isTimeToStopMotor() {
+  //lcd.setCursor(0,2);  lcd.print(measurement_time + measurmentInterval);
+  //lcd.setCursor(0,3);  lcd.print(time_now);
+  time_t time_now = now(); 
+
+  if( time_now >  (start_time + waterInterval) ) { //all in seconds
+    return true;
+  }
+  return false;
+}
+
+
+void syncTimeOnSerial() {
   if(Serial.available() ) {
     processSyncMessage();
   }
@@ -60,20 +128,107 @@ void loop() {
     digitalClockDisplay();  
   }
 
+}
 
-  //Number of milliseconds since the program started
-  time1 = millis();
-  //prints time since program started
-  Serial.println(time1);
+void digitalClockLCD(time_t t, int row) {
+  printLCDDigits(hour(t), 0, row);
+  lcd.setCursor(2,row); 
+  lcd.print(":");
+  printLCDDigits(minute(t), 3, row);
+  lcd.setCursor(5, row);
+  lcd.print(":");
+  printLCDDigits(second(t), 6, row);
+}
+
+void showLastMeasurement(int a0, int a1, int a2,int a3) {
+  lcd.setCursor(0,1);
+  lcd.print("HUM:"); 
+
+  lcd.setCursor(4,1);
+  lcd.print(a0); 
+  lcd.setCursor(8,1);
+  lcd.print(a1); 
+  lcd.setCursor(12,1);
+  lcd.print(a2); 
+  lcd.setCursor(16,1);
+  lcd.print(a3); 
+
+}
 
 
+boolean isTimeToMeasure() {
+  time_t time_now = now(); 
+
+  if( time_now >  (measurement_time + measurmentInterval) ) { //all in seconds
+    measurement_time = time_now;
+    return true;
+  }
+  return false;
+}
+
+void digitalClockDisplay(){
+  // digital clock display of the time
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(" ");
+  Serial.print(month());
+  Serial.print(" ");
+  Serial.print(year()); 
+  Serial.println(); 
+}
+
+void printDigits(int digits){
+  // utility function for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if(digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
+
+void printLCDDigits(int value, int column, int row) {
+  if(value < 10 ) {
+    lcd.setCursor(column, row); 
+    lcd.print("0");
+    lcd.setCursor(column + 1, row); 
+    lcd.print(value);  
+  } 
+  else {
+    lcd.setCursor(column, row); 
+    lcd.print(value); 
+  }
+}
+
+
+void processSyncMessage() {
+  unsigned long pctime;
+  const unsigned long DEFAULT_TIME = 1357041600; // Jan 1 2013
+
+  if(Serial.find(TIME_HEADER)) {
+    pctime = Serial.parseInt();
+    if( pctime >= DEFAULT_TIME) { // check the integer is a valid time (greater than Jan 1 2013)
+      setTime(pctime); // Sync Arduino clock to the time received on the serial port
+    }
+  }
+}
+
+time_t requestSync()
+{
+  Serial.write(TIME_REQUEST);  
+  return 0; // the time will be sent later in response to serial mesg
+}
+
+
+
+void serveTheButtons() {
   if (digitalRead(BUTTON0)==LOW){
     digitalWrite(MOTOR0, HIGH);
   }
   else{
     digitalWrite(MOTOR0, LOW);
   }
-
   if (digitalRead(BUTTON1)==LOW){
     digitalWrite(MOTOR1, HIGH);
   }
@@ -92,67 +247,4 @@ void loop() {
   else{
     digitalWrite(MOTOR3, LOW);
   }
-  int a0 = analogRead(HUMIDITY_PIN0);
-  int a1 = analogRead(HUMIDITY_PIN1);
-  int a2 = analogRead(HUMIDITY_PIN2);
-  int a3 = analogRead(HUMIDITY_PIN3);
-
-
-  lcd.setCursor(0, 0);
-  lcd.print(a0);
-  lcd.setCursor(0, 1);
-  lcd.print(a1);
-  lcd.setCursor(0, 2);
-  lcd.print(a2);
-  lcd.setCursor(0, 3);
-  lcd.print(a3);
 }
-
-void digitalClockDisplay() {
-  // digital clock display of the time
-  Serial.print(hour());
-  printDigits(minute());
-  printDigits(second());
-  Serial.print(" ");
-  Serial.print(day());
-  Serial.print(" ");
-  Serial.print(month());
-  Serial.print(" ");
-  Serial.print(year()); 
-  Serial.println(); 
-}
-
-void printDigits(int digits) {
-  // utility function for digital clock display: prints preceding colon and leading 0
-  Serial.print(":");
-  if(digits < 10)
-    Serial.print('0');
-  Serial.print(digits);
-}
-
-void processSyncMessage() {
-  // if time sync available from serial port, update time and return true
-  while(Serial.available() >=  TIME_MSG_LEN ){  // time message consists of header & 10 ASCII digits
-    char c = Serial.read() ; 
-    Serial.print(c);  
-    if( c == TIME_HEADER ) {       
-      time_t pctime = 0;
-      for(int i=0; i < TIME_MSG_LEN -1; i++){   
-        c = Serial.read();          
-        if( c >= '0' && c <= '9'){   
-          pctime = (10 * pctime) + (c - '0') ; // convert digits to a number    
-        }
-      }   
-      setTime(pctime);   // Sync Arduino clock to the time received on the serial port
-    }  
-  }
-}
-
-time_t requestSync() {
-  Serial.write(TIME_REQUEST);  
-  return 0; // the time will be sent later in response to serial mesg
-}
-
-
-
-
