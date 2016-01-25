761e86c631cc5dd8d8b37448c882a6b5440f96db
magdalena <magdalena.moeller@gmail.com>
2016-01-25 14:46:44 +0100

    plot the vaules

--- givehum2.py
+++ givehum2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 import serial  
 import time
+import subprocess
 
+COL = 2 # display graph for COL sensor
 try:  
     arduino = serial.Serial('/dev/ttyUSB0', 57600)  
 except:  
@@ -9,10 +11,42 @@ except:
 
 time.sleep(2) #seconds
 
-try:    
+garpinof = open('garpinof.dat', 'w')
+
+i = 0;
+while i < 4:
+  try:    
     arduino.write('CGIVEHUM\n')    
     time.sleep(1)  
-    print arduino.readline()  
-    print arduino.readline()  
-except:    
+    str = arduino.readline() 
+    print str 
+  except:    
     print "Failed to send!"  
+
+  list = str.split(';', 5 )
+  print list[COL]
+
+  garpinof.write(list[COL])
+  garpinof.write('\n')
+
+  i = i + 1;
+  time.sleep(5) # delays for 5 seconds
+
+#end of while
+
+garpinof.close();
+
+
+proc = subprocess.Popen(['gnuplot','-p'], 
+                        shell=True,
+                        stdin=subprocess.PIPE,
+                        )
+proc.stdin.write('plot "garpinof.dat" with lines\n')
+proc.stdin.write('set t wxt persist\n') # graph will stay on the screen
+proc.stdin.write('quit\n') #close the gnuplot window
+
+
+
+
+
+
